88b4faa0a9f4a4383a7843ec9b6acda8efc3096d
Christian Le <christianle@berkeley.edu>
2015-04-24 14:39:57 -0700

    sound timing

--- Provocation_3.ino
+++ Provocation_3.ino
@@ -6,6 +6,7 @@
 #define BUSY_PIN  5
 
 const int sampleWindow = 500; // Sample window width in mS (50 mS = 20Hz)
+const int loudThreshold = 4000;
 unsigned int sample;
 
 Wtv020sd16p soundCtrl(RESET_PIN, CLOCK_PIN, DATA_PIN, BUSY_PIN);
@@ -16,7 +17,8 @@ void setup()
    soundCtrl.reset();
 }
 
-double lastReading; 
+double lastReading;
+unsigned long loudStart;
  
 void loop() 
 {
@@ -44,15 +46,24 @@ void loop()
    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
    double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
    
-   if (lastReading > 1.5 && volts < 0.75)
-   {
+   if (lastReading > 1.5 && volts < 0.75) {
      Serial.println("===================");
      Serial.println("   Threshold met   ");
      Serial.println("-------------------");
      Serial.println(volts);
      Serial.println("===================");
      soundCtrl.asyncPlayVoice(0);
-     delay(2000);
+     delay(4000);
+   } else if (lastReading < 0.75 && volts > 1.5) {
+     loudStart = millis();
+   } else if (lastReading > 1.5 && (millis() - loudStart > loudThreshold)) {
+     Serial.println("===================");
+     Serial.println("   Threshold met   ");
+     Serial.println("-------------------");
+     Serial.println(volts);
+     Serial.println("===================");
+     soundCtrl.asyncPlayVoice(1);
+     delay(4000);
    }
    
    lastReading = volts;
